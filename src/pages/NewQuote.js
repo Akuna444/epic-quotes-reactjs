b13987cad6c5dd9bd48b93b109b103f615103de4
Akuna444 <aklilsolomon3@gmail.com>
2022-12-27 11:33:31 +0300

    setting up send request

--- src/pages/NewQuote.js
+++ src/pages/NewQuote.js
@@ -1,13 +1,27 @@
+import { useEffect } from "react";
+import { addQuote } from "../lib/api";
+import useHttp from "../hooks/use-http";
 import QuoteForm from "../components/quotes/QuoteForm";
 import { useHistory } from "react-router-dom";
+import LoadingSpinner from "../components/UI/LoadingSpinner";
 
 const NewQuote = () => {
   const history = useHistory();
-
+  const [sendRequest, status] = useHttp(addQuote);
   const onAddQuoteHandler = (quoteData) => {
-    console.log(quoteData);
-    history.push("/quotes");
+    sendRequest(quoteData);
   };
+
+  useEffect(() => {
+    if (status === "completed") {
+      history.push("/quotes");
+    }
+  }, [status, history]);
+
+  if (status === "pending") {
+    return <LoadingSpinner />;
+  }
+
   return <QuoteForm onAddQuote={onAddQuoteHandler} />;
 };
 
