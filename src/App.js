24a43c313fa0bc7ab5681b375c91ccc462443509
Akuna444 <aklilsolomon3@gmail.com>
2023-01-02 00:50:56 +0300

    add suspense and fallback

--- src/App.js
+++ src/App.js
@@ -1,30 +1,33 @@
-import React from "react";
+import React, { Suspense } from "react";
 import { Redirect, Route, Switch } from "react-router-dom";
 import AllQuotes from "./pages/AllQuotes";
 import Layout from "./components/layout/Layout";
 import NotFound from "./pages/NotFound";
+import LoadingSpinner from "./components/UI/LoadingSpinner";
 const NewQuote = React.lazy(() => import("./pages/NewQuote"));
 const QuoteDetail = React.lazy(() => import("./pages/QuoteDetail"));
 function App() {
   return (
     <Layout>
-      <Switch>
-        <Route path="/quotes/:quoteId">
-          <QuoteDetail />
-        </Route>
-        <Route path="/quotes">
-          <AllQuotes />
-        </Route>
-        <Route path="/new-quote">
-          <NewQuote />
-        </Route>
-        <Route exact path="/">
-          <Redirect to="/quotes" />
-        </Route>
-        <Route path="*">
-          <NotFound />
-        </Route>
-      </Switch>
+      <Suspense fallback={<LoadingSpinner />}>
+        <Switch>
+          <Route path="/quotes/:quoteId">
+            <QuoteDetail />
+          </Route>
+          <Route path="/quotes">
+            <AllQuotes />
+          </Route>
+          <Route path="/new-quote">
+            <NewQuote />
+          </Route>
+          <Route exact path="/">
+            <Redirect to="/quotes" />
+          </Route>
+          <Route path="*">
+            <NotFound />
+          </Route>
+        </Switch>
+      </Suspense>
     </Layout>
   );
 }
